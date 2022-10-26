f6a6326572cb94e22268bece00f663b06cd6751a
crislerwin <crisler.silva@alunos.fael.edu.br>
2022-10-26 08:55:25 -0300

    ✅ cache resorce extra solution 2

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -29,18 +29,28 @@ const SUSPENSE_CONFIG = {
   busyMinDurationMs: 700,
 }
 
-const pokemonResourceCache = {}
+const PokemonResourceCacheContext = React.createContext()
 
-const PokemonResourceCacheContext = React.createContext(getPokemonResource)
+const PokemonCacheProvider = ({children}) => {
+  const cache = React.useRef({})
+  const getPokemonResource = React.useCallback(
+    name => {
+      const lowerName = name.toLowerCase()
+      let resource = cache.current[lowerName]
+      if (!resource) {
+        resource = createPokemonResource(lowerName)
+        cache.current[lowerName] = resource
+      }
+      return resource
+    },
+    [cache],
+  )
 
-function getPokemonResource(name) {
-  const lowerName = name.toLowerCase()
-  let resource = pokemonResourceCache[lowerName]
-  if (!resource) {
-    resource = createPokemonResource(lowerName)
-    pokemonResourceCache[lowerName] = resource
-  }
-  return resource
+  return (
+    <PokemonResourceCacheContext.Provider value={getPokemonResource}>
+      {children}
+    </PokemonResourceCacheContext.Provider>
+  )
 }
 
 function usePokemonResourceCache() {
@@ -98,4 +108,12 @@ function App() {
   )
 }
 
-export default App
+function AppWithProvider() {
+  return (
+    <PokemonCacheProvider>
+      <App />
+    </PokemonCacheProvider>
+  )
+}
+
+export default AppWithProvider
