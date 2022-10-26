920a408328406bd6e72017c79ea054f88a7a0be3
crislerwin <crisler.silva@alunos.fael.edu.br>
2022-10-26 08:45:42 -0300

    ✅ caching context solution

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -31,6 +31,8 @@ const SUSPENSE_CONFIG = {
 
 const pokemonResourceCache = {}
 
+const PokemonResourceCacheContext = React.createContext(getPokemonResource)
+
 function getPokemonResource(name) {
   const lowerName = name.toLowerCase()
   let resource = pokemonResourceCache[lowerName]
@@ -41,6 +43,10 @@ function getPokemonResource(name) {
   return resource
 }
 
+function usePokemonResourceCache() {
+  return React.useContext(PokemonResourceCacheContext)
+}
+
 function createPokemonResource(pokemonName) {
   return createResource(fetchPokemon(pokemonName))
 }
@@ -49,7 +55,7 @@ function App() {
   const [pokemonName, setPokemonName] = React.useState('')
   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
   const [pokemonResource, setPokemonResource] = React.useState(null)
-
+  const getPokemonResource = usePokemonResourceCache()
   React.useEffect(() => {
     if (!pokemonName) {
       setPokemonResource(null)
@@ -58,7 +64,7 @@ function App() {
     startTransition(() => {
       setPokemonResource(getPokemonResource(pokemonName))
     })
-  }, [pokemonName, startTransition])
+  }, [getPokemonResource, pokemonName, startTransition])
 
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
